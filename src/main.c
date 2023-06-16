5ef2860f6d6f1ca1de26194b4aab993d7fa1c88f
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2023-06-16 15:48:06 +0300

    Added motor throttle control

--- src/main.c
+++ src/main.c
@@ -20,24 +20,25 @@ int main() {
 	}
 
 	Motor_Feedback feedback;
-	motor_send_command(motor, 0, 100);
 
 	while(true) {
 		int input_status = steering_check_event(js, &config, &state);
 
 		if (input_status == -1) break;
-		if (input_status != 0) printf("-------------------------------------\n");
+		// if (input_status != 0) printf("-------------------------------------\n");
 
-		if (input_status == 1) {
-			printf("Paddle left: %i\n", state.paddle_left);
-			printf("Paddle right: %i\n", state.paddle_right);
-		} else if (input_status == 2) {
-			printf("Steering state: %i\n", state.steering);
-			printf("Throttle state: %i\n", state.throttle);
-			printf("Brake state: %i\n", state.brake);
-			printf("DPad-L/R: %i\n", state.dpad_horiz);
-			printf("DPad-D/U: %i\n", state.dpad_vert);
-		}
+		// if (input_status == 1) {
+		// 	printf("Paddle left: %i\n", state.paddle_left);
+		// 	printf("Paddle right: %i\n", state.paddle_right);
+		// } else if (input_status == 2) {
+		// 	printf("Steering state: %i\n", state.steering);
+		// 	printf("Throttle state: %i\n", state.throttle);
+		// 	printf("Brake state: %i\n", state.brake);
+		// 	printf("DPad-L/R: %i\n", state.dpad_horiz);
+		// 	printf("DPad-D/U: %i\n", state.dpad_vert);
+		// }
+
+		motor_send_command(motor, state.steering, state.throttle);
 
 		if (motor_receive(motor, &feedback)) {
 			printf("Received motor data:\n");
