3af10891034c9e9c94064ddf0c21b929b991175c
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2023-06-17 01:58:52 +0300

    Read and UART multithreading

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 # I couldn't be bothered to write a makefile
 
+mkdir build
 gcc -Iinc -c -o build/toml.o src/toml.c
-g++ -Iinc -o opi-controller.out \
+g++ -Iinc -std=c++14 -o opi-controller.out \
+	/usr/lib/libatomic.so \
 	build/toml.o \
 	src/logger.c \
 	src/motor.c \
@@ -11,3 +13,5 @@ g++ -Iinc -o opi-controller.out \
 	src/steering.c \
 	src/ctrl_config.c \
 	src/main.cpp
+
+rm -r build
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <time.h>
+#include <atomic>
+#include <thread>
 #include "ctrl_config.h"
 #include "steering.h"
 #include "motor.h"
 #include "logger.h"
-
-static Steering_InputState state;
-static ControllerConfig config;
 
 typedef enum {
 	HOLD,
@@ -15,18 +14,25 @@ typedef enum {
 	DONE
 } InitState;
 
+static std::atomic<Steering_InputState> state;
+static std::atomic<ControllerConfig> config;
+static std::atomic<InitState> input_init_state(HOLD);
+static std::atomic<int> lastInputStatus(0);
+
 void log_input(int input_status) {
 	if (input_status != 0) printf("-------------------------------------\n");
 
+	Steering_InputState state_data = state.load();
+
 	if (input_status == 1) {
-		printf("Paddle left: %i\n", state.paddle_left);
-		printf("Paddle right: %i\n", state.paddle_right);
+		printf("Paddle left: %i\n", state_data.paddle_left);
+		printf("Paddle right: %i\n", state_data.paddle_right);
 	} else if (input_status == 2) {
-		printf("Steering state: %i\n", state.steering);
-		printf("Throttle state: %i\n", state.throttle);
-		printf("Brake state: %i\n", state.brake);
-		printf("DPad-L/R: %i\n", state.dpad_horiz);
-		printf("DPad-D/U: %i\n", state.dpad_vert);
+		printf("Steering state: %i\n", state_data.steering);
+		printf("Throttle state: %i\n", state_data.throttle);
+		printf("Brake state: %i\n", state_data.brake);
+		printf("DPad-L/R: %i\n", state_data.dpad_horiz);
+		printf("DPad-D/U: %i\n", state_data.dpad_vert);
 	}
 }
 
@@ -48,48 +54,33 @@ unsigned long long get_cur_millis() {
 	return millis;
 }
 
-int main() {
-	config_load("ctrlconfig.toml", &config);
-
-	int js = open(config.joy_path, O_RDONLY);
-	int motor = open(config.motor_port, O_RDWR);
-	
-	if (motor_init(motor, config.motor_baudrate) == -1) {
-		log_error("Failed to initialize motor serial port!");
-		return 0;
-	}
-
-	printf("Connected to controller (Baudrate: %i)\n", config.motor_baudrate);
-	Motor_Feedback feedback;
-	init_state(&state);
-	InitState input_init_state = HOLD;
+void fork_input_read(int js_fd) {
 	bool logged_init_info = false;
+	ControllerConfig config_data = config.load();
 
-	int refresh_millis = 100;
-	int refresh_start = 0;
+	while (true) {
+		Steering_InputState curState = state.load();
+		int input_status = steering_check_event(js_fd, &config_data, &curState);
+		InitState cur_init_state = input_init_state.load();
 
-	while(true) {
-		int input_status = steering_check_event(js, &config, &state);
-		// log_input(input_status);
-
-		if (input_init_state != DONE) {
-			if (input_init_state == HOLD) {
+		if (cur_init_state != DONE) {
+			if (cur_init_state == HOLD) {
 				if (!logged_init_info) {
 					printf("Hold down both throttle and brakes simultaneously!\n");
 					logged_init_info = true;
 				}
 
-				if (state.throttle != 0 && state.brake != 0) {
+				if (curState.throttle != 0 && curState.brake != 0) {
 					input_init_state = RELEASE;
 					logged_init_info = false;
 				}
-			} else if (input_init_state == RELEASE) {
+			} else if (cur_init_state == RELEASE) {
 				if (!logged_init_info) {
 					printf("Release both pedals!\n");
 					logged_init_info = true;
 				}
 
-				if (state.throttle == 0 && state.brake == 0) {
+				if (curState.throttle == 0 && curState.brake == 0) {
 					input_init_state = DONE;
 					printf("Initialization done!\n");
 				}
@@ -98,24 +89,55 @@ int main() {
 			continue;
 		}
 
+		state = curState;
+		lastInputStatus = input_status;
+	}
+}
+
+void fork_send_cmd(int motor_fd) {
+	int refresh_millis = 100;
+	int refresh_start = 0;
+	ControllerConfig config_data = config.load();
+
+	while (true) {
+		if (input_init_state.load() != DONE) continue;
+
 		if (
 			get_cur_millis() - refresh_start > refresh_millis
-			|| input_status > 0
+			|| lastInputStatus.load() > 0
 		) {
-			int velocity = state.brake < config.brake_threshold ? 0 : state.throttle;
-			log_state(velocity, state.steering);
-			motor_send_command(motor, state.steering, velocity);
+			Steering_InputState curState = state.load();
+			int velocity = curState.brake < config_data.brake_threshold ? 0 : curState.throttle;
+			log_state(velocity, curState.steering);
+			motor_send_command(motor_fd, curState.steering, velocity);
 
 			refresh_start = get_cur_millis();
 		}
-
-		if (motor_receive(motor, &feedback)) {
-			printf("Received motor data:\n");
-			printf("SpeedR: %i\nSpeedL: %i\nBatVoltage: %i\n",
-				feedback.speedR_meas, feedback.speedL_meas, feedback.batVoltage
-			);
-		}
 	}
+}
+
+int main() {
+	ControllerConfig config_val = config.load();
+
+	config_load("ctrlconfig.toml", &config_val);
+
+	int js = open(config_val.joy_path, O_RDONLY);
+	int motor = open(config_val.motor_port, O_RDWR);
+	
+	if (motor_init(motor, config_val.motor_baudrate) == -1) {
+		log_error("Failed to initialize motor serial port!");
+		return 0;
+	}
+
+	printf("Connected to controller (Baudrate: %i)\n", config_val.motor_baudrate);
+	Motor_Feedback feedback;
+	// init_state(&state);
+
+	std::thread inputThread(fork_input_read, js);
+	std::thread cmdThread(fork_send_cmd, motor);
+
+	inputThread.join();
+	cmdThread.join();
 
 	return 0;
 }
